cfa88d830a5b472cfdb0666e721ad6e97ba67d6b
AliyevaShahana <sahanliyeva@gmail.com>
2024-02-06 16:36:36 +0400

    home-page changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,29 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+// ================================================
+//SLIDER ARROW
 
 let owlNav = document.querySelector(".owl-nav");
 let prev = owlNav.children[0].children[0];
 prev.innerHTML = `<i class="fa-solid fa-arrow-left-long"></i>`;
-// console.log(prev);
-
 let next = owlNav.children[1].children[0];
 next.innerHTML = `<i class="fa-solid fa-arrow-right-long"></i>`;
-console.log(next);
+// ================================================
+// VIDEO
+
+// let videoPlayer = document.querySelector(".video-player");
+// let myVideo = document.querySelector(".my-video");
+// function stopVideo() {}
+// function playVideo() {
+//   videoPlayer.style.display = "block";
+// }
+
+
+ScrollReveal({
+  reset: true,
+  distance: `60px`,
+  duration: 2500,
+  delay: 400,
+});
+// ScrollReveal().reveal(".icon-card", { delay: 100 });
