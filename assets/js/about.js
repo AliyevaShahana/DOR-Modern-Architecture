40ed278a70e5d7daa77eeecbd96a063d980d2c64
AliyevaShahana <sahanliyeva@gmail.com>
2024-02-06 21:53:40 +0400

    about page is ready

---
+++ assets/js/about.js
@@ -0,0 +1,39 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+let owlNav = document.querySelector(".owl-nav");
+let prev = owlNav.children[0].children[0];
+prev.innerHTML = `<i class="fa-solid fa-arrow-left-long"></i>`;
+let next = owlNav.children[1].children[0];
+next.innerHTML = `<i class="fa-solid fa-arrow-right-long"></i>`;
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+valueDisplays.forEach((valueDisplays) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplays.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+// ScrollReveal().reveal(".num", { delay: 300 });
